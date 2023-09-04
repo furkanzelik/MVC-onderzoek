e02a0b0fd3677d41b3c86de794288b07cb9b6b11
Huseyin Furkan Ozcelik <furkanzelik@gmail.com>
2023-09-04 14:28:17 +0200

    Initial commit

---
+++ resources/views/secondroute.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Second Route</title>
+
+
+</head>
+<body>
+
+<?= $secondroute; ?>
+
+
+<a href="/">Back</a>
+</body>
+</html>
